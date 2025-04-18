2f94fa8230a8e462e8ea249476e7eb0a4be06cf9
Coma <comasocean@gmail.com>
2025-04-18 17:54:46 +0900

    250418

---
+++ Bronze4/src/P2083.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class P2083 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+      String[] profile = scanner.nextLine().split(" ");
+      if (profile[0].equals("#")) break;
+
+      if (Integer.parseInt(profile[1]) > 17 || Integer.parseInt(profile[2]) >= 80) {
+        System.out.println(profile[0] + " Senior");
+      } else {
+        System.out.println(profile[0] + " Junior");
+      }
+    }
+  }
+}
---
+++ Bronze4/src/P5596.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class P5596 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int minkookGrade = 0;
+    for (int i = 0; i < 4; i++) {
+      minkookGrade += scanner.nextInt();
+    }
+
+    int mansaeGrade = 0;
+    for (int i = 0; i < 4; i++) {
+      mansaeGrade += scanner.nextInt();
+    }
+
+    System.out.println(Math.max(minkookGrade, mansaeGrade));
+  }
+}
