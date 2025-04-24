5d1d7194bb354a2c7373bb3fd72a6918ee0d8f1d
Coma <comasocean@gmail.com>
2025-04-24 18:55:02 +0900

    250424

---
+++ Bronze4/src/P28431.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P28431 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<Integer> arrayList = new ArrayList<>();
+
+    for (int i = 0; i < 5; i++) {
+      Integer number = scanner.nextInt();
+
+      if (arrayList.contains(number)) {
+        arrayList.remove(number);
+      } else {
+        arrayList.add(number);
+      }
+    }
+
+    System.out.println(arrayList.get(0));
+  }
+}
---
+++ Bronze5/src/P32498.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class P32498 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int testCase = scanner.nextInt();
+    int count = 0;
+    for (int i = 0; i < testCase; i++) {
+      int number = scanner.nextInt();
+      if (number % 2 != 0) count++;
+    }
+
+    System.out.println(count);
+  }
+}
