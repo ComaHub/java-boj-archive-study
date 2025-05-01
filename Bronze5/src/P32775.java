f9e4fccda280a5b165604111e7c635bf7eb5f75a
Coma <comasocean@gmail.com>
2025-05-01 23:15:38 +0900

    250501

---
+++ Bronze5/src/P32775.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class P32775 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int timeOfRail = scanner.nextInt();
+    int timeOfFlight = scanner.nextInt();
+
+    System.out.println(timeOfFlight < timeOfRail ? "flight" : "high speed rail");
+  }
+}
