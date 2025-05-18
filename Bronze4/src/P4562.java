741d5a2e4c201ec06e48fc4bc8f29e991461b015
Coma <comasocean@gmail.com>
2025-05-18 23:45:55 +0900

    250518

---
+++ Bronze4/src/P4562.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class P4562 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int testCase = scanner.nextInt();
+    for (int i = 0; i < testCase; i++) {
+      if (scanner.nextInt() >= scanner.nextInt()) {
+        System.out.println("MMM BRAINS");
+      } else {
+        System.out.println("NO BRAINS");
+      }
+    }
+  }
+}
