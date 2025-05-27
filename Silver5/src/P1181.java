8c8743efb7ea8a4d36b2ded2b7297d5a050a1e62
Coma <comasocean@gmail.com>
2025-05-27 15:40:38 +0900

    250527

---
+++ Silver5/src/P1181.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class P1181 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+    Set<String> set = new HashSet<>();
+    List<String> list;
+
+    int amountWord = Integer.parseInt(bufferedReader.readLine());
+    for (int i = 0; i < amountWord; i++) {
+      set.add(bufferedReader.readLine());
+    }
+
+    list = new ArrayList<>(set);
+    list.sort(new Comparator<String>() {
+      @Override
+      public int compare(String o1, String o2) {
+        if (o1.length() == o2.length()) {
+          return o1.compareTo(o2);
+        } else {
+          if (o1.length() < o2.length()) return -1;
+          else return 1;
+        }
+      }
+    });
+
+    for (String s : list) {
+      System.out.println(s);
+    }
+  }
+}
