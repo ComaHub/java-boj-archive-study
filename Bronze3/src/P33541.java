eebd046060e1520c0f6db3ade9294946626b67d1
Coma <comasocean@gmail.com>
2025-06-01 21:36:59 +0900

    250601

---
+++ Bronze3/src/P33541.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class P33541 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int inputYear = scanner.nextInt();
+
+    for (int i = (inputYear + 1); i <= 9999; i++) {
+      int specialNum = (i / 100) + (i % 100);
+
+      if ((specialNum * specialNum) == i) {
+        System.out.println(i);
+        return;
+      }
+    }
+
+    System.out.println(-1);
+  }
+}
