ad834a0a79c6db700d1e416b3e76809f4653baad
Coma <comasocean@gmail.com>
2025-06-03 18:49:47 +0900

    250603

---
+++ Bronze4/src/P13985.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class P13985 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    String[] line = scanner.nextLine().split(" ");
+    int sumAB = (Integer.parseInt(line[0]) + Integer.parseInt(line[2]));
+    int sumC = Integer.parseInt(line[4]);
+
+    System.out.println(sumAB == sumC ? "YES" : "NO");
+  }
+}
