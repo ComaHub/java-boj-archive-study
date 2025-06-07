a4fd12a506ef0096de990c383fba40b402f9c853
Coma <comasocean@gmail.com>
2025-06-07 23:14:43 +0900

    250607

---
+++ Bronze5/src/P24883.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class P24883 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    if ("N".equalsIgnoreCase(scanner.nextLine())) System.out.println("Naver D2");
+    else System.out.println("Naver Whale");
+  }
+}
