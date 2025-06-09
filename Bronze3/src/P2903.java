436642b5192f6d79f932b539fa24f4dcec3b0cbc
Coma <comasocean@gmail.com>
2025-06-09 13:32:16 +0900

    250609

---
+++ Bronze3/src/P2903.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class P2903 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int num = scanner.nextInt();
+    System.out.println((int) Math.pow(Math.pow(2, num) + 1, 2));
+  }
+}
