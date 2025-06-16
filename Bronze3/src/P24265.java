5a001f59187dda347fa44b4da26f270feee421dc
Coma <comasocean@gmail.com>
2025-06-16 16:45:05 +0900

    250616

---
+++ Bronze3/src/P24265.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P24265 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    long num = scanner.nextLong();
+    System.out.println((num * (num - 1)) / 2);
+    System.out.println(2);
+  }
+}
---
+++ Bronze3/src/P24266.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P24266 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    long num = scanner.nextLong();
+    System.out.println(num * num * num);
+    System.out.println(3);
+  }
+}
