32fa132479534c85b61294b6a1cdafa42a146a30
Coma <comasocean@gmail.com>
2025-07-02 20:29:28 +0900

    250702

---
+++ Bronze4/src/P17548.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P17548 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    String hey = scanner.nextLine();
+    String e = hey.substring(1, hey.length() - 1);
+    System.out.println("h" + e.repeat(2) + "y");
+  }
+}
