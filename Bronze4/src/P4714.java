a949ab65ac57f3a8c38ca7afc0d2c71d2bcd21f2
Coma <comasocean@gmail.com>
2025-07-05 16:12:40 +0900

    250705

---
+++ Bronze4/src/P4714.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class P4714 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+      double height = scanner.nextDouble();
+      if (height == -1.0) break;
+
+      System.out.printf("Objects weighing %.2f on Earth will weigh %.2f on the moon.\n", height, (height * 0.167));
+    }
+  }
+}
