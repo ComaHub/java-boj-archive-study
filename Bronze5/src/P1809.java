f06b724fc616dfcdd2cdaa7671aa882ebd3b16e2
Coma <comasocean@gmail.com>
2025-07-15 23:51:03 +0900

    250715

---
+++ Bronze5/src/P1809.java
@@ -0,0 +1,12 @@
+public class P1809 {
+  public static void main(String[] args) {
+    System.out.println("""
+            (___)
+            (o o)____/
+             @@      \\
+              \\ ____,/
+              //   //
+             ^^   ^^
+             """);
+  }
+}
