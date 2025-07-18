c88731f66a2b415e05923cd86452c90e715c3aa4
Coma <comasocean@gmail.com>
2025-07-18 11:44:34 +0900

    250718

---
+++ Bronze4/src/P15372.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class P15372 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int testCase = scanner.nextInt();
+    for (int i = 0; i < testCase; i++) {
+      long num = scanner.nextLong();
+
+      System.out.println(num * num);
+    }
+  }
+}
