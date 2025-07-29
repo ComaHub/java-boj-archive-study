1b28e14852191b7a22d84404a6199eac246ca3c7
Coma <comasocean@gmail.com>
2025-07-30 00:04:11 +0900

    250729

---
+++ Bronze5/src/P14645.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class P14645 {
+  public static void main(String[] args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+      int stops = scanner.nextInt();
+      int people = scanner.nextInt();
+
+      for (int i = 0; i < stops; i++) {
+        people += scanner.nextInt();
+        people -= scanner.nextInt();
+      }
+    }
+
+    System.out.println("비와이");
+  }
+}
