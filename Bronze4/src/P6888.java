16429e772d552fb7f2e5537c8a843e8c931392b4
Coma <comasocean@gmail.com>
2025-07-31 23:33:53 +0900

    250731

---
+++ Bronze4/src/P6888.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class P6888 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int startYear = scanner.nextInt();
+    int endYear = scanner.nextInt();
+
+    while (startYear <= endYear) {
+      System.out.println("All positions change in year " + startYear);
+      startYear += 60;
+    }
+  }
+}
