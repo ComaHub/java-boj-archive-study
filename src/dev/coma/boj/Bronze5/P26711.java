cd1493cbc5a27b1dd200b6e3a3d6eae3a4f4eb6e
Coma <comasocean@gmail.com>
2025-09-11 00:16:03 +0900

    250911

--- src/dev/coma/boj/Bronze5/P26711.java
+++ src/dev/coma/boj/Bronze5/P26711.java
@@ -1,11 +1,15 @@
 package dev.coma.boj.Bronze5;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class P26711 {
 	public static void main(String[] args) {
 		try (Scanner scanner = new Scanner(System.in)) {
-			System.out.println(scanner.nextInt() + scanner.nextInt());
+			BigInteger numA = new BigInteger(scanner.nextLine());
+			BigInteger numB = new BigInteger(scanner.nextLine());
+			
+			System.out.println(numA.add(numB));
 		}
 	}
 }
