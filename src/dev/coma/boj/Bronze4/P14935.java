c74fc7201048b20c202b6c2e7426f4b2fd8dba92
Coma <comasocean@gmail.com>
2025-11-25 23:49:26 +0900

    251125

---
+++ src/dev/coma/boj/Bronze4/P14935.java
@@ -0,0 +1,13 @@
+package dev.coma.boj.Bronze4;
+
+import java.util.Scanner;
+
+@SuppressWarnings("unused")
+public class P14935 {
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			String input = scanner.nextLine();
+			System.out.println("FA");
+		}
+	}
+}
