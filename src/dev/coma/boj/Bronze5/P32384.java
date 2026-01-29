c0cf33d366ddd089d47fe999f01dc73f17602fae
CoMa <comasocean@gmail.com>
2026-01-30 00:33:42 +0900

    260129

---
+++ src/dev/coma/boj/Bronze5/P32384.java
@@ -0,0 +1,13 @@
+package dev.coma.boj.Bronze5;
+
+import java.util.Scanner;
+
+public class P32384 {
+  public static void main(String[] args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+      int num = scanner.nextInt();
+
+      System.out.print("LoveisKoreaUniversity ".repeat(num));
+    }
+  }
+}
